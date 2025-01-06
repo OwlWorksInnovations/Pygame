3f43c53a601359dc798ec716578263d892826915
owlworksinnovations <juanytfortnite@gmail.com>
2025-01-06 19:12:29 +0200

    added movement

--- ping_pong/app.py
+++ ping_pong/app.py
@@ -5,6 +5,10 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+player_speed = 1000
+
+left_paddle = pygame.Rect(80, 100, 50, 150)
+right_paddle = pygame.Rect(1180, 100, 50, 150)
 
 while running:
     dt = clock.tick(60) / 1000
@@ -22,10 +26,29 @@ while running:
     screen.blit(fps_text_surface, (10, 10))
 
     # Paddles
-    pygame.draw.rect(screen, "white", pygame.Rect(100, 100, 50, 150))
-    pygame.draw.rect(screen, "white", pygame.Rect(1100, 100, 50, 150))
-    # Render game above
+    pygame.draw.rect(screen, "white", left_paddle)
+    pygame.draw.rect(screen, "white", right_paddle)
 
+    keys = pygame.key.get_pressed()
+    left_move_direction = pygame.Vector2(0, 0)
+    right_move_direction = pygame.Vector2(0, 0)
+    
+    if keys[pygame.K_w]:
+        left_move_direction.y = -1
+    if keys[pygame.K_s]:
+        left_move_direction.y = 1
+    if keys[pygame.K_UP]:
+        right_move_direction.y = -1
+    if keys[pygame.K_DOWN]:
+        right_move_direction.y = 1
+
+    left_paddle.move_ip(0, left_move_direction.y * player_speed * dt)
+    right_paddle.move_ip(0, right_move_direction.y * player_speed * dt)
+
+    # Prevent paddles from moving out of the screen
+    left_paddle.y = max(0, min(left_paddle.y, screen.get_height() - left_paddle.height))
+    right_paddle.y = max(0, min(right_paddle.y, screen.get_height() - right_paddle.height))
+    
     pygame.display.flip()
 
 pygame.quit()
